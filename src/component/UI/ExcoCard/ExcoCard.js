1d515cd69b8aa08a95e4eaa724013c7d2408f088
Eugene <eugeneftw500@gmail.com>
2022-12-21 23:27:19 +0800

    Team Animations

--- src/component/UI/ExcoCard/ExcoCard.js
+++ src/component/UI/ExcoCard/ExcoCard.js
@@ -3,49 +3,49 @@ import { Container } from 'react-bootstrap';
 import ReactCardFlip from 'react-card-flip';
 
 const ExcoCard = (props) => {
-    const [hover, setHovering] = useState(false);
+  const [hover, setHovering] = useState(false);
+
+  const onHoverHandler = event => {
+    event.preventDefault();
+    setHovering(true)
+  }
+
+  const onLeaveHandler = event =>{
+    event.preventDefault();
+    setHovering(false)
+  }
   
-    const onHoverHandler = event => {
-      event.preventDefault();
-      setHovering(true)
-    }
-  
-    const onLeaveHandler = event =>{
-      event.preventDefault();
-      setHovering(false)
-    }
+  return (
+
+    //  ${classes.card} ${props.className} > If want to inherit card 
+
+    // Adjusting padding between cards
+    <div className="flex flex-col
+                    px-6 pb-5 ">
     
-    return (
-  
-      //  ${classes.card} ${props.className} > If want to inherit card 
-  
-      // Adjusting padding between cards
-      <div className="flex flex-col
-                      lg:py-5 lg:px-7 px-3 py-2 ">
-      
-          <ReactCardFlip isFlipped={hover} flipDirection="horizontal">
-            {/* Front of Card */}
-            <div> 
-                <img className='rounded-3xl h-96 w-60 m-auto' src={props.image} onMouseMove={onHoverHandler} alt={props.name}/>
-                <div className="text-2xl">{props.name}</div>
-                <div className='pt-1'>{props.tag}</div>
-            </div>
-            {/* Back of Card */}
-              <Container className='flex flex-col object-contain m-auto p-0'>
-                <figure className='relative h-96 w-60 m-auto'>
-                  <img className='rounded-3xl' src={props.image} onMouseLeave={onLeaveHandler} alt={props.name}/>
-                  <figcaption className='absolute h-96 w-60 rounded-3xl top-0 mx-auto px-5 pt-40
-                                         backdrop-blur-xl backdrop-opacity-90
-                                       text-black text-2xl text-center font-bold' 
-                              onMouseLeave={onLeaveHandler}>{props.tag}</figcaption>
-                </figure>
-              </Container> 
-  
-          </ReactCardFlip>
-  
-      </div>
-    );
-  };
-  
-  export default ExcoCard;
-  
+        <ReactCardFlip isFlipped={hover} flipDirection="horizontal">
+          {/* Front of Card */}
+          <div className='flex flex-col justify-center items-center
+                          font-bold text-xl'>
+            <img className='rounded-3xl h-96 w-60 m-auto' src={props.image} onMouseMove={onHoverHandler} alt={props.name}/>
+            <span>{props.name}</span>
+            <span>{props.tag}</span>
+          </div>
+          {/* Back of Card */}
+            <Container className='flex flex-col object-contain m-auto p-0'>
+              <figure className='relative h-96 w-60 m-auto'>
+                <img className='rounded-3xl' src={props.image} onMouseLeave={onLeaveHandler} alt={props.name}/>
+                <figcaption className='absolute h-96 w-60 rounded-3xl top-0 mx-auto px-5 pt-40
+                                       backdrop-blur-xl backdrop-opacity-90
+                                     text-black text-2xl text-center font-bold' 
+                            onMouseLeave={onLeaveHandler}>{props.tag}</figcaption>
+              </figure>
+            </Container> 
+
+        </ReactCardFlip>
+
+    </div>
+  );
+};
+
+export default ExcoCard;
--- src/containers/Team.js
+++ src/containers/Team.js
@@ -1,135 +1,122 @@
-import React, { useEffect } from "react";
-import Card from "../component/UI/Card/Card";
+import {React, useEffect} from 'react'
+import doku from '../images/doku.jpg'
+import Card from '../component/UI/Card/Card'
+import { team } from '../Content'
+import Fade from '../component/Animation/Fade'
+import { motion, useAnimation } from 'framer-motion'
 import { useInView } from "react-intersection-observer";
-import { useAnimation } from "framer-motion";
-import { motion } from "framer-motion";
-import Fade from "../component/Animation/Fade";
-import { team } from "../Content";
-import ExcoCard from "../component/UI/ExcoCard/ExcoCard";
-
-import logo from "../images/logo512.png";
+import ExcoCard from '../component/UI/ExcoCard/ExcoCard'
 
 const Team = (props) => {
-  const control = useAnimation();
-  const [ref, inView] = useInView({threshold: 0});
+
+  const control = useAnimation()
+  const [ref, inView] = useInView()
 
   const variant = {
     hidden: {
       opacity: 0,
     },
     show: {
-      opacity: 1,
+      opacity:1, 
       transition: {
         staggerChildren: 0.8,
-        delayChildren: 0.3,
-      },
-    },
-  };
+        delayChildren: 0.3
+      }
+    }
+  }
 
   const delay = {
     hidden: {
       opacity: 0,
-      x: -100,
-    },
+      x:-100
+    }, 
     show: {
       opacity: 1,
-      x: 0,
+      x:0, 
       transition: {
-        duration: 0.8,
-      },
-    },
-  };
+        duration: 0.8
+      }
+    }
+  }
 
   useEffect(() => {
     if (inView) {
       control.start("show");
-    } else {
-      control.start("hidden");
+    } else{
+      control.start("hidden")
     }
   }, [control, inView]);
 
-  return (
-    
-    <div className="pt-20 md:pt-64 lg:pt-16" id="team">
-      <Card className="overflow-hidden justify-center">
-        <ul className="list-none px-0 md:px-16 lg:px-32">
-          <Fade
-            direction="bottom"
+  return (  
+    <div className="pt-20 lg:pt-16" id="team">
+      <Card className='overflow-hidden'>
+        <ul className='list-none px-0 md:px-16 lg:px-32'>
+
+          <Fade 
+            direction = "bottom"
             speed="1.5"
             delay="0.2"
-            className="font-bold text-5xl text-center
-                       bg-transparent mb-4"
-          >
-            {team.title}
+            className='font-bold text-5xl text-center mb-4'>
+            {team.title}                  
           </Fade>
-
-          <Fade
+            
+          <Fade 
             direction="bottom"
             speed="1.2"
             delay="0.5"
-            className="font-bold text-xl text-center mb-5"
-          >
-            {team.opening}
+            className='font-bold text-xl text-center mb-5'>
+              {team.opening}
           </Fade>
-
-          <motion.ul
-            ref={ref}
+        
+          <motion.ul 
+            ref = {ref}
             variants={variant}
             initial="hidden"
             animate={control}
-            className="list-none pl-0 flex flex-wrap m-auto justify-center"
-          >
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.p.name} tag={team.p.tag} />
-            </motion.li>
-            
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.vp.name} tag={team.vp.tag} />
-            </motion.li>
+            className='list-none pl-0 flex flex-wrap m-auto justify-center'>
 
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.p.name} tag={team.p.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.vp.name} tag={team.vp.tag}/>
+              </motion.li>
+              
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.cp.name1} tag={team.cp.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.cp.name2} tag={team.cp.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.ocgl.name} tag={team.ocgl.tag}/>
+              </motion.li>
+              
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.pnp.name} tag={team.pnp.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.log.name} tag={team.log.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.bmag.name} tag={team.bmag.tag}/>
+              </motion.li>
+
+              <motion.li variants={delay}> 
+                <ExcoCard image={doku} name={team.fincon.name} tag={team.fincon.tag}/>
+              </motion.li>
           </motion.ul>
-
-          <motion.ul
-            ref={ref}
-            variants={variant}
-            initial="hidden"
-            animate={control}
-            className="list-none pl-0 flex flex-wrap mx-5 justify-center"
-          >
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.cp.name1} tag={team.cp.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.cp.name2} tag={team.cp.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.ocgl.name} tag={team.ocgl.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.bmag.name} tag={team.bmag.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.pnp.name} tag={team.pnp.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.fincon.name} tag={team.fincon.tag} />
-            </motion.li>
-
-            <motion.li variants={delay}>
-              <ExcoCard image={logo} name={team.log.name} tag={team.log.tag} />
-            </motion.li>
-
-          </motion.ul>
+          
         </ul>
       </Card>
-    </div>
-  );
-};
 
-export default Team;
+    </div>
+  )
+}
+
+export default Team
