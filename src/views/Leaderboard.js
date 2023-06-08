4df7f4e5cc91206e9b6d3f4acdfc9173739c15aa
Eugene Wee <eugeneftw500@gmail.com>
2023-06-09 02:04:12 +0800

    Added logic for leaderboard, null user

--- src/views/Leaderboard.js
+++ src/views/Leaderboard.js
@@ -2,40 +2,56 @@ import { ParallaxBannerLayer } from "react-scroll-parallax"
 import bg from '../images/background.png'
 import Board from "../component/UI/Board/Board"
 import NeonButton from "../component/UI/Button/NeonButton";
+import { getAuth } from "@firebase/auth";
+import { Link } from "react-router-dom";
 
-const Leaderboard = () => {
-  return (
-    <ParallaxBannerLayer className="h-screen w-screen">
-      <ParallaxBannerLayer
-        image={bg}
-      />
-      <ParallaxBannerLayer
-        className="
+const Leaderboard = (props) => {
+    const currentUser = getAuth().currentUser;
+
+    return (
+        <ParallaxBannerLayer className="h-screen w-screen">
+            <ParallaxBannerLayer
+                image={ bg }
+            />
+            <ParallaxBannerLayer
+                className="
             font-blackopsone text-center
             mt-40 mx-4">
-        <h1
-            className="mb-8 items-center
+                <h1
+                    className="mb-8 items-center
             tracking-[4px] lg:text-4xl sm:text-3xl
             text-white">
-                LEADERBOARD
-        </h1>
-        <Board/>
-          <div className="mt-4">
+                    LEADERBOARD
+                </h1>
 
-        <NeonButton clas
-            >
-            May the best OG win!
-        </NeonButton>
-          </div>
-          <p
-              className="mt-3 items-center
-            tracking-widest lg:text-xl sm:text-[8px]
-            text-white">
-              Mystery Challenges await you...
-          </p>
-      </ParallaxBannerLayer>
-    </ParallaxBannerLayer>
-  )
+                { currentUser ? (<div>
+                        <Board/>
+
+                        <div className="mt-4">
+                            <NeonButton>
+                                May the best OG win!
+                            </NeonButton>
+                        </div>
+                        <p
+                            className="mt-3 items-center
+                            tracking-widest lg:text-xl sm:text-[8px]
+                            text-white">
+                            Mystery Challenges await you...
+                        </p>
+                    </div>
+                ) : (
+                    <div className="px-10">
+                        <Link to="/login">
+                            <NeonButton>
+                                Login to View Leaderboard!
+                            </NeonButton>
+                        </Link>
+                    </div>
+                )
+                }
+            </ParallaxBannerLayer>
+        </ParallaxBannerLayer>
+    );
 }
 
 export default Leaderboard
